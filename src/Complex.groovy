2a0a55e19b1533f9c3047c11e025d8e345570a0e
sbt-kamalova-ip <IPKamalova@sberbank.ru>
2018-01-12 15:24:40 +0300

    Test overriding plus operator

---
+++ src/Complex.groovy
@@ -0,0 +1,36 @@
+class Complex {
+    // z = ai + b
+    private def a, b
+
+    Complex(a, b) {
+        this.a = a
+        this.b = b
+    }
+
+    Complex plus(Complex c) {
+        return new Complex(this.a + c.a, this.b + c.b)
+    }
+
+    @Override
+    boolean equals(Object object) {
+        if (object != null && object instanceof Complex) {
+            Complex complex = (Complex) object;
+            return complex.a == this.a && complex.b == this.b
+        }
+        else return false;
+    }
+
+    @Override
+    String toString() {
+        return "(" + a + "i + " + b + ")"
+    }
+
+    static void main(String[] args) {
+        def c1 = new Complex(1, 2);
+        def c2 = new Complex(3, 4);
+        def c3 = new Complex(4, 6);
+        assert c1 + c2 == c3
+    }
+
+
+}
